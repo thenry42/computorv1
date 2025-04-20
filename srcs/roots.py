a86f9a8c499b240e66b6aec9a17d5863d880e8e9
thenry <thenry@student.42.fr>
2025-04-20 21:05:32 +0200

    base for calculus

--- srcs/roots.py
+++ srcs/roots.py
@@ -11,12 +11,20 @@ def get_roots(terms: list[Term], delta: float):
 
 
 def get_only_root(terms: list[Term]):
+    # x = -b / 2a
+
     return 0
 
 
 def get_two_roots(terms: list[Term]):
+    # x1 = (-b-sqrt(delta)) / 2a
+    # x2 = (-b+sqrt(delta)) / 2a
+    
     return 0
 
 
 def get_complex_roots(terms: list[Term]):
+    # complex roots can be found using the formulas above
+    # BUT it requires to use i (imginary numbers)
+    # i = sqrt(-1) <=> i^2 = -1
     return 0
